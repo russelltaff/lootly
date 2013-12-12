ba3b8699a8e8d1ea079c9f5fff0c0c2238334779
Russell Taff <rtaff2012@gmail.com>
2013-12-12 17:12:34 -0500

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lootly::Application.config.secret_key_base = '67dc8f5178795a5d442c4d0a7e01594a17b90ad3a5e284968e9e2fff2b391bd687a29842dc6a5d09db5601450b62f419b7bd895b640c2f2aeb12de9892c35ea0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lootly::Application.config.session_store :cookie_store, key: '_lootly_session'
